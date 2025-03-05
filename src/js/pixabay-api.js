dbe8299fa9fdb5eb55bd357b9e8d2aa5335ffe55
Pavlo Sheremet <115708207+pavlo-sheremet-dev@users.noreply.github.com>
2025-03-05 15:15:42 +0000

    without page

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,10 +3,10 @@ const API_KEY = '47404996-12b53dec464063fd6255bb496';
 
 export const PER_PAGE = 12;
 
-export const fetchImages = async (query, page = 1) => {
+export const fetchImages = async query => {
   try {
     const { data } = await axios.get(
-      `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}&safesearch=true`
+      `https://pixabay.com/api/?q=${query}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}&safesearch=true`
     );
     return data;
   } catch (error) {
