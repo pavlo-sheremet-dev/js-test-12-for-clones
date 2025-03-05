b4dae6670c20a4604fd07f9aca759d7958a80c08
Pavlo Sheremet <115708207+pavlo-sheremet-dev@users.noreply.github.com>
2025-03-05 15:21:02 +0000

    without await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,10 +6,10 @@ export const PER_PAGE = 12;
 
 export const fetchImages = async (query, page = 1) => {
   try {
-    const { data } = await axios.get(
+    const response = axios.get(
       `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}&safesearch=true`
     );
-    return renderGallery(data.hits);
+    return response.data;
   } catch (error) {
     console.log(error.message);
     throw error;
